1d8c1907f847d3c3da3d7cf116968a0cf381456a
Ian Page Hands <iphands@gmail.com>
2015-07-18 17:42:36 +0000

    for realz tempo, and killable player thread

--- giantguitar/__init__.py
+++ giantguitar/__init__.py
@@ -1,39 +1,58 @@
 #!/usr/bin/python
 
+import sys
 import os
 import time
+import signal
+import atexit
 from Queue import Queue
 
 from reader import Reader 
 from sounder import Sounder
 from player import Player
 
+q = {}
+
 def main():
-  delay = .1
+  global q
+  delay = .01
   lights = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
 
   reader = Reader(lights)  
   sounder = Sounder()
   player = Player(sounder)
-  q = player.song("polly")
+  q = player.song("ripple")
 
   while True:
+    debug_str = ""
     time.sleep(delay)
     reader.fetch()
     #os.system('clear')
     
-    if not q.empty():
-      player.chord(q.get_nowait())
+    if not q["play"].empty():
+      player.chord(q["play"].get_nowait())
 
     for ch in lights:
-      if lights[ch] > 3 or ch == 4:
+      debug_str += "{}:{} ".format(ch, lights[ch])
+      if lights[ch] > 900:
         sounder.start(ch+1)
       else:
         sounder.stop(ch+1)
-      #print("{}: {}".format(ch, lights[ch]))
-    #print("--------------------------------------------")
+      # print("{}: {}".format(ch, lights[ch]))
+    # print("--------------------------------------------")
+    print(debug_str)
     time.sleep(delay)
-              
+
+def sig_handler(sig, frame):
+  cleanup()
+
+def cleanup():
+  print("\n\nShutting Giant Guitar with Fricken Laser Beams down...\n\n")
+  q["control"].put("stop")
+  sys.exit(0)
+
 if __name__ == "__main__":
+  signal.signal(signal.SIGINT, sig_handler)
+  atexit.register(cleanup)
   main()
 
--- giantguitar/player.py
+++ giantguitar/player.py
@@ -8,7 +8,7 @@ class Player(object):
         self.sounder = sounder
         with open('chords.js') as file:
             self.chords = json.load(file)
-        print(self.chords)
+        #print(self.chords)
 
     def chord(self, k):
         print (self.chords[k])
@@ -16,19 +16,28 @@ class Player(object):
             s = int(s_str)
             self.sounder.fret(s, self.chords[k][s_str])
 
+    def check_song(self, name, song):
+        for e in song["chords"]:
+            if not e[0] in self.chords:
+                raise Exception("Song {} asks for chord {} but that chord is not defined in chords.js".format(name, e[0]))
+
     def song(self, name):
         with open("songs/" + name + ".js") as file:
-            q = Queue()
             song = json.load(file)
+            self.check_song(name, song)
+            q = { "play": Queue(), "control": Queue() }
             threading.Thread(target=worker, args=(q, name, song)).start()
             return q
 
 def worker(q, name, song):
-    print("Playing: {} at tempo {}".format(name, song["tempo"]))
     tempo = int(song["tempo"])
+    whole = (60000 / tempo) * 4
+    print("\n\nPlaying: {} at tempo {} (a whole note is {}ms)\n\n".format(name, tempo, whole))
     for e in song["chords"]:
-        print(e)
-        q.put(e[0])
-        time.sleep((tempo / 1000.0) * e[1])
+        if not q["control"].empty():
+            if q["control"].get_nowait() == "stop":
+                break
+        q["play"].put(e[0])
+        time.sleep((whole / 1000.0) * e[1])
         
         
