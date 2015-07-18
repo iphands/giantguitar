4b33f4fdb5d6de16929d145651fe8497ea36daf2
Ian Page Hands <iphands@gmail.com>
2015-07-18 05:58:01 +0000

    added audio player class

--- giantguitar/__init__.py
+++ giantguitar/__init__.py
@@ -3,20 +3,30 @@
 import os
 import time
 from reader import Reader 
+from player import Player
 
 def main():
-  delay = .1
+  delay = .03
   lights = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
+
   reader = Reader(lights)  
+  player = Player()
   
   while True:
     time.sleep(delay)
     reader.fetch()
     os.system('clear')
+
     for ch in lights:
-        print("{}: {}".format(ch, lights[ch]))
+      if lights[ch] < 300:
+        player.stop(ch+1)
+      else:
+        player.start(ch+1)
+
+      print("{}: {}".format(ch, lights[ch]))
     print("--------------------------------------------")
     time.sleep(delay)
               
 if __name__ == "__main__":
-    main()
+  main()
+
